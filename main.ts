e0ba8a56d0a478e09d4e8b8be9b83d7332ab6412
default <natnael.debesay@stu.ocsb.ca>
2025-10-01 17:23:35 +0000

    main.ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program calculates the weight in pounds of my friend's 34.5 kilogram dog.
+ */
+
+// shows conversion
+console.log("34.5kg * 2.205 = " + String(34.5 * 2.205));
+
+console.log("\nDone");
