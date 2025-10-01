07fb5adc4b27d48436aeda898239becac650d8b9
default <natnael.debesay@stu.ocsb.ca>
2025-10-01 17:28:17 +0000

    main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program converts 34.5 kilograms to pounds.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//shows conversion
+	fmt.Println("34.5 * 2.205 = " + fmt.Sprint(34.5*2.205))
+
+	fmt.Println("\nDone.")
+}
